0dc0f0fdf4431670d3120d9db3f31fc08a4ee939
Ahmed Ali <ahmedcali1984@gmail.com>
2025-03-26 18:10:55 +0300

    Add main source file

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "queue.h"
+
+int main(int argc, char **argv)
+{
+    if (argc < 2) {
+        printf("Usage: %s [MESSAGE].\n", argv[0]);
+        return 1;
+    }
+    
+    for (int i = 1; i < argc; ++i) {
+        printf("%s ", argv[i]);
+    }
+    printf("\n");
+    return 0;
+}
