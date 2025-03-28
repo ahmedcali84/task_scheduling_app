a5c6126892faa2788d36ab3db1f53941e06251f8
Ahmed Ali <ahmedcali1984@gmail.com>
2025-03-28 22:56:44 +0300

    Update Main File

--- main.c
+++ main.c
@@ -1,15 +1,35 @@
-#include "queue.h"
+#include "task.h"
 
 int main(int argc, char **argv)
 {
     if (argc < 2) {
-        printf("Usage: %s [MESSAGE].\n", argv[0]);
+        Log_Out(ERROR, "Usage: %s [MESSAGE].\n", argv[0]);
         return 1;
     }
+
+    const char *file_path = "output.bin";
     
-    for (int i = 1; i < argc; ++i) {
-        printf("%s ", argv[i]);
+    Queue queue;
+    if (!InitQueue(&queue)) return 1;
+
+    // Load existing tasks
+    if (!load_file(file_path, &queue)) {
+        Log_Out(WARN, "No Existing Tasks Found. Starting Fresh.\n");
     }
-    printf("\n");
+
+    char *buffer = extract_args(argc, argv);
+    if (!Enqueue(&queue, buffer)) {
+        Log_Out(ERROR, "Failed To Enqueue message: %s.\n", buffer);
+        free(buffer);
+        return 1;
+    }
+
+    free(buffer);
+
+    // save new tasks
+    if (!dump_queue(file_path, &queue)) return 1;
+    PrintQueue(&queue);
+
+    DeallocateQueue(&queue);
     return 0;
 }
