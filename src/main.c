69e494e312aebe27283487398a65266aca24a30b
Ahmed Ali <ahmedcali1984@gmail.com>
2025-04-02 21:55:47 +0300

    Remove extract_args

--- src/main.c
+++ src/main.c
@@ -1,14 +1,5 @@
 #include "task.h"
 
-#define SIXTEEN "             "
-
-void usage(char **argv)
-{
-    FILE *out = stdout;
-    fprintf(out,"USAGE: %s <Task Message> <Priority>.\n",argv[0]);
-    fprintf(out,"    Priority: \n      0 -> LOW \n      1 -> MEDIUM \n      2 -> HIGH\n");
-}
-
 int main(int argc, char **argv)
 {
     if (argc < 2) {
@@ -16,13 +7,8 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    if (strcmp(argv[1], "--help") == 0) {
-        usage(argv);
-        return 0;
-    }
-
     const char *file_path = "output.bin";
-    
+
     Queue queue;
     if (!InitQueue(&queue)) return 1;
 
@@ -31,23 +17,21 @@ int main(int argc, char **argv)
         Log_Out(WARN, "No Existing Tasks Found. Starting Fresh.\n");
     }
 
-    char *buffer = extract_args(argc, argv);
-    if (buffer == NULL) return 1;
+    Task task = parse_args(&queue, argc , argv);
 
-    if (task_exists(&queue, buffer)) return 1;
+    if (task_exists(&queue, task.message)) return 1;
 
-    if (!Enqueue(&queue, buffer, atoi(argv[3]))) {
-        Log_Out(ERROR, "Failed To Enqueue message: %s.\n", buffer);
-        free(buffer);
+    if (!Enqueue(&queue, task.message, task.priority)) {
+        Log_Out(ERROR, "Failed To Enqueue message: %s.\n", task.message);
+        free(task.message);
         return 1;
     }
-
-    free(buffer);
 
     // save new tasks
     if (!dump_queue(file_path, &queue)) return 1;
     PrintQueue(&queue);
 
+    free(task.message);
     DeallocateQueue(&queue);
     return 0;
 }
